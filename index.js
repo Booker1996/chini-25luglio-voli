250c620f7af6539b6cc244b633bcc49f56ae6de5
Booker1996 <lorenzo.chini@studenti.unitn.it>
2018-07-25 02:32:03 -0700

    Scritto le funzioni getFlightInfo, getFlights, setFlights

--- index.js
+++ index.js
@@ -6,15 +6,22 @@
         minuti di ritardo
  */
 function getFlightInfo(flightCode) {
-    console.log("Hello World");
+    var lunVoli = voli.length;
+    for (var i=0; i<lunVoli; i++){
 
-
-    var flightInfo = {
-        departureAirPort: 'MXP',
-        arrivalAirport: 'JFK',
-        delay: 37
+        if(voli[i].flightCode==flightCode){
+            var fligthInfo = {
+                departureAirport: voli[i].departureAirport,
+                arrivalAirport: voli[i].arrivalAirport,
+                delay: voli[i].delay
+            };
+            return fligthInfo;
+        }
     }
-    console.log(flightInfo);
+    return "Errore! Volo non trovato!";
+
+
+    //console.log(flightInfo);
 }
 
 /*
@@ -25,15 +32,57 @@ function getFlightInfo(flightCode) {
     delle quali rappresenta un codice di volo. ([SQ388,UA928,...])
  */
 function getFlights(airportCode, selector) {
+    var lunVoli = voli.length;
+    var listavoli = [];
+    if(selector=="departure"){
+        for(var i = 0; i<lunVoli; i++){
+            if(voli[i].departureAirport==airportCode){
+                listavoli.push(voli[i].flightCode);
+            }
+        }
+    }else if(selector=="arrival"){
+        for(var i = 0; i<lunVoli; i++){
+            if(voli[i].arrivalAirport==airportCode){
+                listavoli.push(voli[i].flightCode);
+            }
+        }
+    }
+    return listavoli;
+
 
 }
-
 
 /*
     Crea un nuovo volo
  */
-function setFlights() {
-
+function setFlights(flightCode, departureAirport, arrivalAirport, delay) {
+    var volo ={
+        flightCode: flightCode,
+        departureAirport: departureAirport,
+        arrivalAirport: arrivalAirport,
+        delay: delay
+    }
+    voli.push(volo);
 }
 
-getFlightInfo();
+
+var voli = [];
+voli[0] = {
+    flightCode: 'SQ388',
+    departureAirport: 'MXP',
+    arrivalAirport: 'JFK',
+    delay: 37
+};
+voli[1] = {
+    flightCode: 'UA928',
+    departureAirport: 'MXP',
+    arrivalAirport: 'CIA',
+    delay: 89
+};
+
+console.log(getFlightInfo("SQ388"));
+//setFlights("CI400","BOH","CIA",12);
+console.log(getFlights("MXP","departure"));
+console.log(getFlights("CIA","arrival"));
+console.log(getFlights("MXP","arrival"));
+console.log(getFlights("MXP","dsd"));
