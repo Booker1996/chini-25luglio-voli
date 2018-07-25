b7059e2bcbb266084e8bfff688592de6e0fb32ec
Booker1996 <lorenzo.chini@studenti.unitn.it>
2018-07-25 01:34:39 -0700

    Inizializzo le funzioni

--- index.js
+++ index.js
@@ -1,6 +1,39 @@
-
-function prova() {
+/*
+    Dato un codice di volo (es: SQ388) restituisce un oggetto che contiene le
+    informazioni riguardo:
+        aereoporto di partenza
+        aereoporto di arrivo
+        minuti di ritardo
+ */
+function getFlightInfo(flightCode) {
     console.log("Hello World");
+
+
+    var flightInfo = {
+        departureAirPort: 'MXP',
+        arrivalAirport: 'JFK',
+        delay: 37
+    }
+    console.log(flightInfo);
 }
 
-prova();
+/*
+    getFlights(airportCode, selector)
+    Dato un aeroporto ed un selettore (una stringa che può prendere il valore
+    "departure" o "arrival"), restituisci i codici di tutti i voli in partenza
+    o in arrivo da quell'aereoporto (ritorna quindi un array di stringhe, ognuna
+    delle quali rappresenta un codice di volo. ([SQ388,UA928,...])
+ */
+function getFlights(airportCode, selector) {
+
+}
+
+
+/*
+    Crea un nuovo volo
+ */
+function setFlights() {
+
+}
+
+getFlightInfo();
